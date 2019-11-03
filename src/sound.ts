7d5fbf8819f66557b9957fbbceb96b1f29d88bd1
Matthias Löffel <matthias.loeffel@swisstxt.ch>
2019-11-03 02:51:53 +0100

    only one annoying beep at once

--- src/sound.ts
+++ src/sound.ts
@@ -2,12 +2,18 @@ const audioContext = new AudioContext()
 const gain = audioContext.createGain()
 gain.connect(audioContext.destination)
 
-export const beep = (vol: number, freq: number, duration: number) => {
-    gain.gain.value = vol * 0.01
-    const oscillator = audioContext.createOscillator()
-    oscillator.connect(gain)
-    oscillator.frequency.value = freq
-    oscillator.type = "square"
-    oscillator.start(audioContext.currentTime)
-    oscillator.stop(audioContext.currentTime + duration * 0.001)
+let beeping = false
+
+export const beep = (vol: number, freq: number, duration: number): void => {
+    if (!beeping) {
+        beeping = true
+        gain.gain.value = vol * 0.01
+        const oscillator = audioContext.createOscillator()
+        oscillator.addEventListener('ended', () => beeping = false)
+        oscillator.connect(gain)
+        oscillator.frequency.value = freq
+        oscillator.type = "square"
+        oscillator.start(audioContext.currentTime)
+        oscillator.stop(audioContext.currentTime + duration * 0.001)
+    }
 }
